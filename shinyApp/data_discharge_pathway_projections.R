578459aecccffc1b04515ff9f103eac07a0d239c
nick howlett <nhowlett87@gmail.com>
2024-01-31 09:35:42 +0000

    forgot data prep script

--- shinyApp/data_discharge_pathway_projections.R
+++ shinyApp/data_discharge_pathway_projections.R
@@ -30,15 +30,16 @@ FROM MODELLING_SQL_AREA.dbo.discharge_pathway_projections'
 data_dpp <- sqlQuery(con, query)
 report_date <- ymd(data_dpp$report_date)[1]
 
-levels <- c("Not tomorrow")
-levels <- set_names(levels, glue::glue("Still CTR {report_date + ddays(1)}"))
-
 data_dpp <- data_dpp %>%
   mutate(across(matches('date'), ~ as.POSIXct(.x, tz = 'UTC'))) %>%
-  mutate(pathway = factor(pathway, levels = 
-    c("Other", "P1", "P2", "P3", "Not tomorrow"),
-    labels = c("NTCR but not on D2A queue","P1 queue", "P2 queue", "P3 queue", names(levels))
+  mutate(pathway = factor(pathway, levels =
+    c("Other", "P1", "P2", "P3 / Other Complex Discharge"),
+    labels = c("NCTR but not on D2A queue",
+               "Additional P1",
+               "Additional P2",
+               "Additional P3")
   )) %>%
+  mutate(site = factor(recode(site, bri = "BRI", nbt = "NBT", weston = "Weston"), levels = c("NBT", "BRI", "Weston"))) %>% 
   # mutate(pathway = fct_recode(pathway,  !!!levels)) %>%
   # mutate(pathway = fct_recode(pathway,  "NTCR but not on D2A queue" = "Other")) %>%
   pivot_wider(names_from = metric,
@@ -47,5 +48,5 @@ data_dpp <- data_dpp %>%
     tooltip_n = glue::glue("{pathway} = {round(n, 0)}"),
     tooltip_n_noqueue = glue::glue("{str_remove_all(pathway, 'queue')} = {round(n, 0)}"),
     tooltip_errorbar = glue::glue("({round(u95,0)}, {round(l95,0)})")
-  ) %>%
-  mutate(pathway = fct_relevel(pathway, names(levels), after = 0))
+  ) #%>%
+  # mutate(pathway = fct_relevel(pathway, names(levels), after = 0))
