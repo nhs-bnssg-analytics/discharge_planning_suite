eb7861ceb0471368d96c93b735bb8c7332503a95
Nick Howlett <nhowlett87@gmail.com>
2025-01-22 10:25:06 +0000

    Update code_new_admits.R

--- code_new_admits.R
+++ code_new_admits.R
@@ -1,5 +1,4 @@
 df_new_admit <- local({ 
-  browser()
   if(seed) set.seed(123)
   # report_date <- report_start -ddays(1) # (DEPRECATED)
   report_date <- report_start
@@ -41,55 +40,62 @@ df_new_admit <- local({
   sites <- unique(df_admit_fcast_flt$site)
   dates <- sort(unique(df_admit_fcast_flt$date))
   
+  
   sim <- expand_grid(site = sites,
                      rep = seq_len(n_rep),
                      date = dates) %>%
     left_join(df_admit_fcast_flt, join_by(site, date == date)) %>%
     rowwise() %>%
     mutate(#fcast_samp = rnorm(1, mean = fcast, sd = get_sd_from_ci(ci = c(l_85, u_85))),
-           # arrivals = pmax(extraDistr::rdnorm(1, mean = (1.16*fcast)+0.5),0)
-           arrivals = pmax(extraDistr::rdnorm(1, mean = (fcast)+0.5, sd = get_sd_from_ci(ci = c(l_85, u_85))))
-           ) %>%
+      # arrivals = pmax(extraDistr::rdnorm(1, mean = (1.16*fcast)+0.5),0)
+      arrivals = pmax(extraDistr::rdnorm(
+        1, mean = (fcast) + 0.5, sd = get_sd_from_ci(ci = c(l_85, u_85))
+      ))) %>%
     ungroup() %>%
-    mutate(
-     # arrivals = coalesce(map_dbl(fcast_samp, rpois, n = 1), 0),
-           # coalesce in case we sample below zero
-           day = rep(0:n_days, length(sites) * n_rep)) %>% 
+    mutate(# arrivals = coalesce(map_dbl(fcast_samp, rpois, n = 1), 0),
+      # coalesce in case we sample below zero
+      day = rep(0:n_days, length(sites) * n_rep)) %>%
     filter(arrivals > 0) %>%
     left_join(rdist) %>%
     # (DEPRECATED) los plus one because it is stored as the left boundary of the interval censor (i.e. 0-2 should los 1)
     # mutate(los = map(arrivals, function(arr) rdist(arr) + 1)) %>%
-    mutate(los = map2(arrivals, rdist, function(arr, dist_fn) dist_fn(arr))) %>%
+    mutate(los = map2(arrivals, rdist, function(arr, dist_fn)
+      dist_fn(arr))) %>%
     # mutate(los = map(arrivals, function(arr) rdist(arr)) %>% map(\(x) map_dbl(x, ~sample(seq(.x, .x + 2), 1)))) %>%
     unnest(los) %>%
     mutate(date_end = date + ddays(los)) %>%
+    mutate(los = cut(
+      los,
+      breaks = c(0, 3, 4, 5, 6, 7, 8, 9, 10, Inf),
+      include.lowest = TRUE
+    )) %>%
+    left_join(props_site) %>%
     # DEPRECATED CHUNK #
     # # mutate(date_end = date + ddays(pmax(los-1, 0))) %>%
     # #(DEPRECATED) day is + 1 to shift all predicted discharges to the next snapshot
     # # group_by(site, day = lubridate::interval(report_date, date_end)/ddays(1), rep) %>%
     # # day is + 1 as an interval of zero for new arrivals on day x that are NCTR on x should counted on should be on day x + 1 (day 1 events are on day 1 etc)
     # # group_by(site, day = lubridate::interval(report_date, date_end)/ddays(1) + 1, rep) %>%
-    
     # pmax here to keep the (small) numbers discharged on day zero in the sim
     # and we add one because interval of zero corresponds to day 'one' of the
     # simulation
-    group_by(site, day = pmax(1, lubridate::interval(report_date, date_end)/ddays(1) + 1), rep) %>%
+    group_by(site, day = pmax(1, lubridate::interval(report_date, date_end) /
+                                ddays(1) + 1), rep, los, props) %>%
     count() %>%
     ungroup() %>%
-    left_join(props_site) %>%
-    mutate(pathways = map2(n, props, ~ factor(sample(names(.y),
-                                                     size = .x,
-                                                     prob = .y,
-                                                     replace = TRUE),
-                                              levels = names(.y))))  %>%
-    mutate(pathways = map(pathways, table)) %>%
-    unnest_wider(pathways) %>%
-    dplyr::select(-n, -props) %>%
-    pivot_longer(
-      cols = -c(site, day, rep),
-      names_to = "pathway",
-      values_to = "count"
-    ) %>%
-    mutate(count = as.numeric(count), source = "new_admits")
+    mutate(pathways = map2(n, props, ~ factor(
+      sample(
+        names(.y),
+        size = .x,
+        prob = .y,
+        replace = TRUE
+      ), levels = names(.y)
+    ))) %>%
+    unnest(pathways) %>%
+    group_by(site, day, rep, pathway = pathways) %>%
+    count(name = "count") %>%
+    ungroup() %>%
+    complete(site, day, rep, pathway, fill = list(count = 0)) %>%
+    mutate(source = "new_admits")
   sim
 })
