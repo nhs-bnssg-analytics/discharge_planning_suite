a1993ec2ca2adea4c2a0f1303352cc99a7fd5e2c
Nick Howlett <nhowlett87@gmail.com>
2025-01-29 17:02:41 +0000

    updates

--- scratch/scratch_validation_investigation_plots_extra.R
+++ scratch/scratch_validation_investigation_plots_extra.R
@@ -1,3 +1,117 @@
+library(fitdistrplus)
+library(tidyverse)
+library(tidymodels)
+library(lubridate)
+
+con <- switch(.Platform$OS.type,
+              windows = RODBC::odbcConnect(dsn = "xsw"),
+              unix = {"/root/sql/sql_connect_string_linux" |>
+                  readr::read_lines() |>
+                  RODBC::odbcDriverConnect()}
+)
+
+# con <- RODBC::odbcDriverConnect(readr::read_lines("/root/sql/sql_connect_string_linux"))
+
+source("utils/utils.R")
+source("utils/theme.R")
+source("utils/colour_functions.R")
+
+validation_end <- ymd("2024-09-01")
+validation_start <- ymd("2023-07-01")
+# start_date <- validation_end - dweeks(13
+
+fc_train_length_wks <- 10
+
+start_date <- validation_start + dweeks(fc_train_length_wks) # use full period save ARIMA training period
+
+seed <- FALSE
+plot_int <- FALSE
+
+n_rep <- 1E1
+
+run_date <- today()
+n_days <- 10
+
+# latest nctr data
+nctr_df_full <-
+  RODBC::sqlQuery(
+    con,
+    glue::glue("SELECT
+       [RN]
+      ,[Organisation_Code_Provider]
+      ,[Organisation_Code_Commissioner]
+      ,[Census_Date]
+      ,[Month_Date]
+      ,[Month]
+      ,[Day_Of_Week]
+      ,[Week_Start_Date]
+      ,[NHS_Number]
+      ,[Person_Stated_Gender_Code]
+      ,[Person_Age]
+      ,[CDS_Unique_Identifier]
+      ,[Sub_ICB_Location]
+      ,[Organisation_Site_Code]
+      ,[Current_Ward]
+      ,[Specialty_Code]
+      ,[Bed_Type]
+      ,[Date_Of_Admission]
+      ,[BNSSG]
+      ,[Local_Authority]
+      ,[Criteria_To_Reside]
+      ,[Date_NCTR]
+      ,[Current_LOS]
+      ,[Days_NCTR]
+      ,[Days_NCTR_On_Current_Code]
+      ,[Current_Delay_Code]
+      ,[Local_Authority_grouped]
+      ,[Site_Name]
+      ,[Current_Delay_Code_Standard]
+      ,[Current_Delay_Code_Detailed]
+      ,[Acute Community split]
+      ,[Current_Covid_Status]
+      ,[Planned_Date_Of_Discharge]
+      ,[Date_Toc_Form_Completed]
+      ,[Toc_Form_Status]
+      ,[Discharge_Pathway]
+      ,[DER_File_Name]
+      ,[DER_Load_Timestamp]
+  FROM Analyst_SQL_Area.dbo.vw_NCTR_Status_Report_Daily_JI
+  Where Census_Date <= '{run_date}'"
+    ))
+
+
+pathway_recodes <- c(
+  "Pathway 3 - Other" = "P3",
+  "Awaiting confirmation MDT" = "Other",
+  "Awaiting referral to SPA" = "Other",
+  "Pathway 3 - D2A" = "P3",
+  "Pathway 0" = "Other",
+  "Pathway 1 - D2A" = "P1",
+  "Awaiting confirmation Social" = "Other",
+  "Pathway 2 - Other" = "P2",
+  "Pathway 2 - D2A" = "P2",
+  "Pathway 2" = "P2",
+  "Pathway 2  Safeguarding concern" = "P2",
+  "Pathway 2   Specialist  eg BIRU" = "P2",
+  "Awaiting confirmation Other" = "Other",
+  "Pathway 1 - Other" = "P1",
+  "Pathway 1" = "P1",
+  "P3 / Other Complex Discharge" = "P3",
+  "Pathway 3 / Other Complex Discharge" = "P3",
+  "Uncoded" = "Other",
+  "Repatriation" = "Other",
+  "NCTR Null" = "Other",
+  "Not Set" = "Other",
+  "18a  Infection  bxviii  Standard" = "Other",
+  "xviii. Awaiting discharge to a care home but have not had a COVID 19 test (in 48 hrs preceding discharge)." = "Other",
+  "15b  Repat  bxv  WGH" = "Other",
+  "Meets Criteria to Reside" = "Other"
+)
+
+
+out <- readRDS("data/final_validation_full_out_1e3_newpropsloslogic.RDS")
+
+
 census_log <- nctr_df_full %>%
   filter(Person_Stated_Gender_Code %in% 1:2) %>%
   mutate(nhs_number = as.character(NHS_Number),
@@ -74,8 +188,21 @@ calc_cur_admit_disch_rdy <- function(d, h = 10, census_log) {
     mutate(day = factor(seq_len(h)), .by = c(site, pathway))
   }
 
-calc_new_admit_disch_rdy(dates[5], census_log = census_log)
-calc_cur_admit_disch_rdy(dates[5], census_log = census_log)
+
+dates <- nctr_df_full  %>%
+  filter(between(Census_Date, validation_start, validation_end-ddays(1)))%>%
+  filter(Census_Date >= start_date,
+         Census_Date < validation_end,
+         Census_Date > ymd("2023-07-01"), # data before this are spurious
+         Census_Date < max(Census_Date) - ddays(n_days),
+         # Data not submitted for UHBW on this day
+         Census_Date != ymd("2024-07-17"),
+         # remove dates near Christmas
+         abs(lubridate::interval(Census_Date, ymd("2023-12-25"))/ddays(1)) > 15
+  ) %>%
+  pull(Census_Date) %>%
+  unique()
+
 
 observed <- map(dates, \(d) list(
   new_admits = calc_new_admit_disch_rdy(d, census_log = census_log),
@@ -230,6 +357,74 @@ bind_rows(
        colour = "",
        y = str_wrap("RMSE ratio between baseline and simulation models", 50)) +
   theme(legend.position = "bottom")
+
+bind_rows(
+  out %>%
+    map("result") %>%
+    map("na_out_df") %>%
+    bind_rows(.id = "id") %>%
+    filter(site != "nbt") %>%
+    complete(nesting(id, site, day, date), source, metric, pathway, fill = list(n = 0)) %>%
+    dplyr::select(id, site, day, pathway, source, n) %>%
+    pivot_wider(values_from = n, names_from = source) %>%
+    mutate(source = "new_admits"),
+  out %>%
+    map("result") %>%
+    map("ca_out_df") %>%
+    bind_rows(.id = "id") %>%
+    filter(site != "NBT") %>%
+    complete(nesting(id, site, day, date), source, metric, pathway, fill = list(n = 0)) %>%
+    dplyr::select(id, site, day, pathway, source, n) %>%
+    pivot_wider(values_from = n, names_from = source) %>%
+    mutate(source = "cur_admits")
+) %>%
+  dplyr::select(-observed) %>%
+  left_join(observed) %>%
+  summarise(
+    simulated = sum(simulated),
+    observed = sum(observed),
+    .by = c(id, site, pathway)
+  ) %>%
+  left_join(
+    out %>%
+      map("result") %>%
+      map("bl_out_df") %>%
+      bind_rows(.id = "id") %>%
+      mutate(day = factor(day, levels = 1:10)) %>%
+      filter(site != "nbt") %>%
+      complete(nesting(id, site, day, date), source, metric, pathway, fill = list(n = 0)) %>%
+      rename(baseline = n) %>%
+      summarise(baseline = sum(baseline), .by = c(id, site, pathway))
+  ) %>%
+  filter(site != "system") %>%
+  mutate(across(c(simulated, baseline), list(error = \(x) observed - x))) %>%
+  mutate(across(matches("_error"), list(sq = \(x) x^2))) %>%
+  summarise(across(matches("error_sq"), list(mean = \(x) mean(x, na.rm = TRUE)
+                                             # uq = \(x) quantile(x, 0.975, na.rm = TRUE),
+                                             # lq = \(x) quantile(x, 0.025, na.rm = TRUE)
+                                             )), .by = c(site, id, pathway)) %>%
+  mutate(across(matches("error_sq_"), list(root = sqrt))) %>%
+  mutate(
+    ratio = baseline_error_sq_mean_root/simulated_error_sq_mean_root,
+    ) %>%
+  summarise(across(ratio, list(mean = \(x) median(x, na.rm = TRUE),
+                               uq = \(x) quantile(x, 0.75, na.rm = TRUE),
+                               lq = \(x) quantile(x, 0.25, na.rm = TRUE))
+                   ),
+            .by = c(site, pathway)
+  ) %>%
+  ggplot(aes(x = "", y = ratio_mean, ymin = ratio_lq, ymax = ratio_uq)) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  geom_pointrange() +
+  theme_minimal() +
+  facet_grid(site ~ pathway) +
+  ggh4x::facet_grid2(pathway ~ site, scales = "free_y", independent = "y") +
+  labs(x = "Day",
+       colour = "",
+       y = str_wrap("RMSE ratio between baseline and simulation models", 50)) +
+  theme(legend.position = "bottom")
+
+
 
 bind_rows(
   out %>%
